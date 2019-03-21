97959119347194b6124d453e0a8ed45cf3903930
Kamil Grela <kagrela@gmail.com>
2019-03-21 23:03:02 +0100

    Refactor tests.

--- src/test/fetch-config/action-creators.test.tsx
+++ src/test/fetch-config/action-creators.test.tsx
@@ -5,60 +5,46 @@ import {
     fetchConfigFailure
 } from '../../js/fetch-config/actions-creators';
 import nock from 'nock';
-import {FetchConfigAction} from '../../js/fetch-config/types';
 
 beforeEach(() => {
     nock.cleanAll();
 });
 
-type Dummy = {}
 
 it('should run with success', async () => {
     nock('http://localhost:80')
         .get('/page/initialdata')
         .reply(200, {});
-    const dispatched: Array<FetchConfigAction<Dummy>> = [];
-    const dispatch = (action: FetchConfigAction<Dummy>) => {
-        dispatched.push(action);
-    };
+    const dispatch = jest.fn();
     await fetchConfig()(dispatch);
-    expect(dispatched).toMatchSnapshot();
+    expect(dispatch.mock.calls).toMatchSnapshot();
 });
 
 it('should fail to parse response', async () => {
     nock('http://localhost:80')
         .get('/page/initialdata')
         .reply(200, 'test');
-    const dispatched: Array<FetchConfigAction<Dummy>> = [];
-    const dispatch = (action: FetchConfigAction<Dummy>) => {
-        dispatched.push(action);
-    };
+    const dispatch = jest.fn();
     await fetchConfig()(dispatch);
-    expect(dispatched).toMatchSnapshot();
+    expect(dispatch.mock.calls).toMatchSnapshot();
 });
 
 it('should fail on 400', async () => {
     nock('http://localhost:80')
         .get('/page/initialdata')
         .reply(400);
-    const dispatched: Array<FetchConfigAction<Dummy>> = [];
-    const dispatch = (action: FetchConfigAction<Dummy>) => {
-        dispatched.push(action);
-    };
+    const dispatch = jest.fn();
     await fetchConfig()(dispatch);
-    expect(dispatched).toMatchSnapshot();
+    expect(dispatch.mock.calls).toMatchSnapshot();
 });
 
 it('should fail on 500', async () => {
     nock('http://localhost:80')
         .get('/page/initialdata')
         .reply(500);
-    const dispatched: Array<FetchConfigAction<Dummy>> = [];
-    const dispatch = (action: FetchConfigAction<Dummy>) => {
-        dispatched.push(action);
-    };
+    const dispatch = jest.fn();
     await fetchConfig()(dispatch);
-    expect(dispatched).toMatchSnapshot();
+    expect(dispatch.mock.calls).toMatchSnapshot();
 });
 
 it('should create fetch config request', () => {
