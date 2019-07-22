641fe93f8625de2cf4f82a8879d46357af1d4f53
Kamil Grela <kagrela@gmail.com>
2019-07-22 14:42:28 +0200

    Handle fetch config success properly.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## [3.2.11] - 2019-07-22
+### Fixed
+- Handling fetch config
+
 ## [3.2.10] - 2019-07-22
 ### Fixed
 - Bring back null in react components
--- src/js/fetch-config/reducers.tsx
+++ src/js/fetch-config/reducers.tsx
@@ -1,16 +1,21 @@
 import {FetchConfigAction, FetchConfigState, FETCH_CONFIG} from './types';
+import {User} from '../user/types';
 
 const initialState = {
     isLoading: false,
     data: undefined,
 };
 
-export const fetchConfigReducer = <T extends {}>(
+export const fetchConfigReducer = <T extends { user: User }>(
     state: FetchConfigState<T> = initialState,
     action: FetchConfigAction<T>): FetchConfigState<T> => {
     switch (action.type) {
         case FETCH_CONFIG.REQUEST:
             return {...state, isLoading: true, data: undefined};
+        case FETCH_CONFIG.SUCCESS:
+            const data = {...action.data};
+            delete data.user;
+            return {...state, isLoading: false, data};
         case FETCH_CONFIG.FAILURE:
             return {...state, isLoading: false, data: undefined};
         default:
